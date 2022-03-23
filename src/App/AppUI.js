a4b46877c0edf01a75cfade495e3608eaa828844
Rocco <rtarantino@fi.uba.ar>
2022-03-22 22:19:03 -0300

    Uso de LocalStorage, estados de Loading y error con useEffect

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -5,13 +5,28 @@ import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
 
-function AppUI({totalToDos,completedToDos,searchValue,setSearchValue,searchedToDos,completeToDo,deleteToDo}){
+function AppUI({
+    totalToDos,
+    completedToDos,
+    searchValue,
+    setSearchValue,
+    searchedToDos,
+    completeToDo,
+    deleteToDo,
+    loading,
+    error}) {
+
     return (
         <React.Fragment>
             <TodoCounter total={totalToDos} completed={completedToDos}/>
             <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue}/>
             
             <TodoList>
+
+                {error && <p>Hubo un error al cargar los datos!</p>}
+                {loading && <p>Cargando...</p>}
+                {(!loading && !searchedToDos.length) && <p>¡Crea tu primer ToDo!</p>}
+
                 {searchedToDos.map(todo => (
                 <TodoItem 
                     key={todo.text} 
--- src/App/index.js
+++ src/App/index.js
@@ -3,15 +3,66 @@
 import React from 'react';
 import {AppUI} from './AppUI';
 
-const defaultListaToDo = [
+/*const defaultListaToDo = [
   {text: 'Hacer mercado', completed: false},
   {text: 'Botar basura', completed: false},
   {text: 'Comprar mueble', completed: false},
-];
+];*/
+
+function useLocalStorage(itemName, initialValue) {
+
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(false);
+  const [item, setItem] = React.useState(initialValue);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      try{
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
+
+        if (!localStorageItem){
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+        }
+
+        setItem(parsedItem);
+        setLoading(false);
+      } catch(error) {
+        setError(error);
+      }
+
+    }, 3000);
+  });
+
+  const saveItem = (newItem) => {
+    try{
+      const strItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, strItem);
+      setItem(newItem);
+    }catch(error){
+      setError(error);
+    }
+  };
+
+  return {
+    item,
+    saveItem,
+    loading,
+    error,
+  };
+
+}
 
 function App() {
 
-  const [todoList, setToDos] = React.useState(defaultListaToDo);
+  const {
+    item: todoList,
+    saveItem: saveToDos,
+    loading,
+    error} = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedToDos = todoList.filter(todo => !!todo.completed).length;
@@ -23,28 +74,30 @@ function App() {
     searchedToDos = todoList;
   } else {
     searchedToDos = todoList.filter(todo => {
-      const todoText = todo.text.toLowerCase();
-      const searchText = searchValue.toLowerCase();
-      return todoText.includes(searchText);
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return todoText.includes(searchText);
     });
   }
 
   const completeToDo = (text) => {
     const todoIndex = todoList.findIndex(todo => todo.text === text);
-    const newToDos = [...todoList];
-    newToDos[todoIndex].completed = true;
-    setToDos(newToDos);
+    const newItem = [...todoList];
+    newItem[todoIndex].completed = true;
+    saveToDos(newItem);
   };
 
   const deleteToDo = (text) => {
     const todoIndex = todoList.findIndex(todo => todo.text === text);
-    const newToDos = [...todoList];
-    newToDos.splice(todoIndex, 1);
-    setToDos(newToDos);
+    const newItem = [...todoList];
+    newItem.splice(todoIndex, 1);
+    saveToDos(newItem);
   };
 
   return (
-    <AppUI 
+    <AppUI
+      error={error}
+      loading={loading}
       totalToDos={totalToDos}
       completedToDos={completedToDos}
       searchValue={searchValue}
